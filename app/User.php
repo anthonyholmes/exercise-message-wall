410d65d0dc5319eb2114ad3be6f4ced584ecc832
Anthony Holmes <anthony@antlur.co>
2018-07-30 16:00:56 -0500

    Add message relationship to user

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Message;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -26,4 +27,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the messages that belong to the user
+     */
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
